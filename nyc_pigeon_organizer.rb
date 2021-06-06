7758894f5698089c5b30e316b08ad1a4d5b9e024
abstract-instructions-1268 <pranik.chainani@yale.edu>
2021-06-06 22:55:25 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,24 @@
 def nyc_pigeon_organizer(data)
   # write your code here!
+  pigeon_list = {}
+  names = []
+  
+  data[:color].each do |key, value|
+    names.push(value)
+  end
+  
+  names = names.flatten.uniq
+  
+  names.each do |name|
+    data.each do |attribute, type|
+      type_list = []
+      type.each do |key, arr|
+        if arr.include? name
+          type_list.push(key)
+        end
+      end
+      pigeon_list[name][attribute] = type_list
+    end
+  end
+  pigeon_list
 end
